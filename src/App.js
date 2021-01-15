8debe1f209171f3a3021b95e63c6ccb44146c3cf
ab.reza <a.re.morteza@gmail.com>
2021-01-16 02:39:32 +0330

    init parse

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import "./assets/styles/App.css";
+import './assets/styles/App.css';
 
-import { StylesProvider } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
-import { SnackbarProvider } from "notistack";
-import React from "react";
-import { connect } from "react-redux";
-import { IntlProvider } from "react-redux-multilingual";
+import { StylesProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { SnackbarProvider } from 'notistack';
+import Parse from 'parse';
+import React from 'react';
+import { connect } from 'react-redux';
+import { IntlProvider } from 'react-redux-multilingual';
 
-import Notifier from "./components/Notifications/Notifications.jsx";
-import Root from "./root/Root.jsx";
-import MuiTheme from "./theme/MuiTheme";
-import RTLMuiTheme from "./theme/RTLMuiTheme";
-import translations from "./translations";
-import jss from "./utils/jssRTL";
+import Notifier from './components/Notifications/Notifications.jsx';
+import Root from './root/Root.jsx';
+import MuiTheme from './theme/MuiTheme';
+import RTLMuiTheme from './theme/RTLMuiTheme';
+import translations from './translations';
+import jss from './utils/jssRTL';
+
+let PARSE_SERVER_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost/parse_server'
+    : 'http://81.31.169.15/parse_server';
+
+let liveQueryServerURL =
+  process.env.NODE_ENV === 'development'
+    ? 'ws://localhost/ws/'
+    : 'ws://81.31.169.15/ws/';
+Parse.initialize('asdfEWFkej2l3kj2lfjasfjasdf9', 'AKjdfkebfj323k238s9dfsdf');
+Parse.serverURL = PARSE_SERVER_URL;
+Parse.Parse.liveQueryServerURL = liveQueryServerURL;
 
 const AppRout = () => (
   <SnackbarProvider>
@@ -26,7 +40,7 @@ const App = ({ dir }) => {
 
   return (
     <IntlProvider translations={translations}>
-      {dir === "rtl" ? (
+      {dir === 'rtl' ? (
         <ThemeProvider theme={RTLMuiTheme}>
           <StylesProvider jss={jss}>
             <AppRout />
@@ -42,7 +56,7 @@ const App = ({ dir }) => {
 };
 
 const mapStateToProps = (state) => ({
-  dir: state.Intl.locale === "fa" ? "rtl" : "ltr",
+  dir: state.Intl.locale === 'fa' ? 'rtl' : 'ltr',
 });
 
 export default connect(mapStateToProps)(App);
