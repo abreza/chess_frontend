9819078216b8513e713cc120e87040eaf0a3e06e
ab.reza <a.re.morteza@gmail.com>
2021-01-20 11:30:25 +0330

    revert local serer url

--- src/parse/init.js
+++ src/parse/init.js
@@ -3,12 +3,12 @@ import Parse from 'parse';
 export const initParseServer = () => {
   let PARSE_SERVER_URL =
     process.env.NODE_ENV === 'development'
-      ? 'https://chesss.rastaiha.ir/parse_server'
+      ? 'http://localhost/parse_server'
       : 'https://chesss.rastaiha.ir/parse_server';
 
   let liveQueryServerURL =
     process.env.NODE_ENV === 'development'
-      ? 'wss://chesss.rastaiha.ir/ws/'
+      ? 'ws://localhost/ws/'
       : 'wss://chesss.rastaiha.ir/ws/';
   Parse.initialize('asdfEWFkej2l3kj2lfjasfjasdf9', 'AKjdfkebfj323k238s9dfsdf');
   Parse.serverURL = PARSE_SERVER_URL;
