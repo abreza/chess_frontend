83113cf3a29e3fb2c5446c460574177737d155e7
ab.reza <a.re.morteza@gmail.com>
2021-01-20 09:07:54 +0330

    remove LoginDialog

--- src/components/Dialog/LoginDialog.jsx
+++
@@ -1,8 +0,0 @@
-import { Dialog } from '@material-ui/core';
-import React from 'react';
-
-function LoginDialog({ open, handleClose }) {
-  return <Dialog open={open} onClose={handleClose}></Dialog>;
-}
-
-export default LoginDialog;
